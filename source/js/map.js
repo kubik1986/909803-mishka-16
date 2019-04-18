e0f993c2adcb0d580b71f1c2865cbaa46c42ceaf
Viacheslav Akulov <sentence@ngs.ru>
2019-04-18 12:39:13 +0700

    Добавил скрипт интерактивной карты

---
+++ source/js/map.js
@@ -0,0 +1,33 @@
+var mapHeightMobile = 457;
+var minPadding = 40;
+var clientHeight = document.documentElement.clientHeight;
+var map = document.querySelector('.contacts__map');
+
+if (clientHeight < mapHeightMobile + minPadding * 2) {
+  mapHeightMobile = (clientHeight - minPadding * 2) + 'px'
+  map.style.height = mapHeightMobile;
+}
+
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map('map', {
+    center: [59.938631, 30.323055],
+    zoom: 17,
+    controls: ['zoomControl', 'fullscreenControl']
+  });
+  myMap.behaviors.disable(['scrollZoom']);
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    myMap.behaviors.disable('drag');
+  }
+
+  var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/icon-map-pin.svg',
+    iconImageSize: [66, 100],
+    iconImageOffset: [-25, -110],
+    // iconShadow: true,
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
