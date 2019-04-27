e83293dc9530689f1849abe7f936a243814a4cf9
Viacheslav Akulov <sentence@ngs.ru>
2019-04-27 11:12:03 +0700

    Обновил скрипт карты

--- source/js/app/map.js
+++ source/js/app/map.js
@@ -2,9 +2,10 @@ var mapHeightMobile = 457;
 var minPadding = 40;  // отступ, необходимый для возможности прокрутки карты на устройстве
 var clientHeight = document.documentElement.clientHeight;
 var map = document.querySelector('.contacts__map');
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 // Проверка условия, что карта не занимает по высоте весь экран. При необходимости высота карты уменьшается для возможности ее свободной прокрутки
-if (clientHeight < mapHeightMobile + minPadding * 2) {
+if (isMobile && clientHeight < mapHeightMobile + minPadding * 2) {
   mapHeightMobile = (clientHeight - minPadding * 2) + 'px'
   map.style.height = mapHeightMobile;
 }
@@ -21,7 +22,7 @@ function init() {
     suppressMapOpenBlock: true
   });
   myMap.behaviors.disable(['scrollZoom']);
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  if (isMobile) {
     myMap.behaviors.disable('drag');
   }
 
